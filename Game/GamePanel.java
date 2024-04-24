48fac8be949995231998b51a211b76cc454c82ed
carlitia <105894626+carlawiselowski@users.noreply.github.com>
2024-04-24 16:17:32 -0300

    sunset update

--- Game/GamePanel.java
+++ Game/GamePanel.java
@@ -8,7 +8,7 @@ public class GamePanel extends JPanel{
     //screen setting
     int originalPixelSize = 16; // 16x16 tile
     int scale = 3;
-    int raius = 300;
+    int raius = 150;
 
     int pixelSize = originalPixelSize * scale; // 48x48 tile
     int maxScreenColun = 16; // tiles horizontally
@@ -28,12 +28,14 @@ public class GamePanel extends JPanel{
     protected void paintComponent(Graphics g){
         super.paintComponent(g);
         //g.setColor(new Color(160,82,45)); //brown - sienna 
-        g.setColor(Color.blue);
+        g.setColor(new Color(173, 216, 230)); //light blue
         g.fillRect(0, 0, screenWidth, screenHeight/2);
         //x = 768/2 - 150 | y= 576/2 - 150 | diametro = 300
         g.setColor(Color.yellow);
-        g.fillOval(230, 140, 300, 300);
+        g.fillOval(screenWidth/2 - raius, screenHeight/2 - raius, raius+raius, raius+raius);
 
+        g.setColor(new Color(126, 205, 50)); //green
+        g.fillRect(0, screenHeight/2, screenWidth, screenHeight/2);
 
     }
 
